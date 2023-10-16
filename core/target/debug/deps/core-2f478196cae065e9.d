cf1e9ac2084c30226616d3640f059289db0dfc54
darcy <acednes@gmail.com>
2023-10-16 22:45:29 +1100

    remove target

--- core/target/debug/deps/core-2f478196cae065e9.d
+++
@@ -1,9 +0,0 @@
-/home/darcy/code/ibex/core/target/debug/deps/core-2f478196cae065e9.rmeta: src/lib.rs src/compose.rs src/dom.rs src/render.rs src/route.rs
-
-/home/darcy/code/ibex/core/target/debug/deps/core-2f478196cae065e9.d: src/lib.rs src/compose.rs src/dom.rs src/render.rs src/route.rs
-
-src/lib.rs:
-src/compose.rs:
-src/dom.rs:
-src/render.rs:
-src/route.rs:
--- core/target/debug/deps/core-7ae69cd22294ccbe.d
+++
@@ -1,9 +0,0 @@
-/home/darcy/code/ibex/core/target/debug/deps/core-7ae69cd22294ccbe.rmeta: src/lib.rs src/compose.rs src/dom.rs src/render.rs src/route.rs
-
-/home/darcy/code/ibex/core/target/debug/deps/core-7ae69cd22294ccbe.d: src/lib.rs src/compose.rs src/dom.rs src/render.rs src/route.rs
-
-src/lib.rs:
-src/compose.rs:
-src/dom.rs:
-src/render.rs:
-src/route.rs:
