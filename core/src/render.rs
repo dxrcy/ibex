a36709417a2639e0796b8b80c4a979f35462a53b
darcy <acednes@gmail.com>
2023-10-17 19:50:08 +1100

    comments

--- core/src/render.rs
+++ core/src/render.rs
@@ -1,6 +1,7 @@
 use crate::compose::{Attribute, Tag};
 use crate::dom::{Document, DomElement, DomNode};
 
+/// Render a `Document` to a HTML string
 pub fn render(page: Document) -> String {
     if !page.head.attributes.is_empty() || !page.body.attributes.is_empty() {
         panic!("Cannot use attributes on <head> or <body> tags (how did you even get this error?)");
@@ -24,6 +25,7 @@ pub fn render(page: Document) -> String {
     )
 }
 
+/// Render multiple DOM nodes
 fn render_nodes(nodes: Vec<DomNode>) -> String {
     nodes
         .into_iter()
@@ -31,7 +33,7 @@ fn render_nodes(nodes: Vec<DomNode>) -> String {
         .collect::<Vec<_>>()
         .join("")
 }
-
+/// Render a single DOM node
 fn render_node(node: DomNode) -> String {
     match node {
         DomNode::Element(element) => render_element(element),
@@ -39,6 +41,7 @@ fn render_node(node: DomNode) -> String {
     }
 }
 
+/// Render a DOM element to HTML string
 fn render_element(element: DomElement) -> String {
     match element.tag {
         Tag::Meta | Tag::Link | Tag::Img | Tag::Input | Tag::Br | Tag::Hr => {
@@ -69,11 +72,13 @@ fn render_element_with_children(element: DomElement) -> String {
     )
 }
 
+/// Render attributes in key="value" format
 fn format_attributes(attributes: Vec<Attribute>) -> String {
     if attributes.is_empty() {
         return String::new();
     }
 
+    // Space to separate from tag name
     " ".to_string()
         + &attributes
             .into_iter()
