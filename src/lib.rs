9063f65f7ba2131c192ed8b980b4f72701c6c50c
darcy <acednes@gmail.com>
2023-11-15 16:09:57 +1100

    make `extras` public again, but dont re-export everything

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,10 @@
 /// Some built in features
-mod extras;
+pub mod extras;
 /// Routing and file management for SSG apps
 pub mod ssg;
 
 pub use core::*;
-pub use extras::*;
+pub use extras::{is_local, use_meta, Meta};
 pub use macros::view;
 
 pub mod prelude {
