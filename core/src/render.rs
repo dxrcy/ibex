c88400210c3c1c7362a606a33072e18e0a5f3ca0
darcy <acednes@gmail.com>
2024-01-06 14:04:58 +1100

    fix void tag rendering

--- core/src/render.rs
+++ core/src/render.rs
@@ -1,4 +1,4 @@
-use crate::compose::{Attribute, Tag};
+use crate::compose::Attribute;
 use crate::dom::{Document, DomElement, DomNode};
 
 /// Render a `Document` to a HTML string
@@ -47,34 +47,23 @@ fn render_node(node: DomNode) -> String {
 
 /// Render a DOM element to HTML string
 fn render_element(element: DomElement) -> String {
-    match element.tag {
-        Tag::Meta | Tag::Link | Tag::Img | Tag::Input | Tag::Br | Tag::Hr => {
-            render_element_self_closing(element)
+    if element.tag.is_void() {
+        if !element.children.is_empty() {
+            panic!("Void tag <{}> cannot contain children", element.tag);
         }
-        _ => render_element_with_children(element),
+        format!(
+            "<{tag}{attrs}>",
+            tag = element.tag,
+            attrs = format_attributes(element.attributes),
+        )
+    } else {
+        format!(
+            "<{tag}{attrs}>{content}</{tag}>",
+            tag = element.tag,
+            attrs = format_attributes(element.attributes),
+            content = render_nodes(element.children),
+        )
     }
-}
-
-/// Render an element with no innerHTML. Eg. `<br/>`
-fn render_element_self_closing(element: DomElement) -> String {
-    if !element.children.is_empty() {
-        panic!("Cannot use children on <{}/> tag", element.tag);
-    }
-    format!(
-        "<{tag}{attrs}{slash}>",
-        tag = element.tag,
-        attrs = format_attributes(element.attributes),
-        slash = if element.tag.is_void() { "" } else { "/" },
-    )
-}
-/// Render an element with tag that supports innerHTML. Eg. `<a>...</a>`
-fn render_element_with_children(element: DomElement) -> String {
-    format!(
-        "<{tag}{attrs}>{content}</{tag}>",
-        tag = element.tag,
-        attrs = format_attributes(element.attributes),
-        content = render_nodes(element.children),
-    )
 }
 
 /// Render attributes in key="value" format
